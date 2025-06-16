772b18af98d1f828b0f0770ba0e2b4394cb93ffe
Nikita Sanjay Yadav <nikitayadav2102@gmail.com>
2025-06-17 00:09:55 +0530

    Add files via upload

---
+++ javaScript/function.js
@@ -0,0 +1,10 @@
+function greetUser(user)
+{
+    let str= "Welcome, "+user+"!";
+    return str;
+}
+const square=(n)=>n*n;
+
+console.log(greetUser("Nikita"));
+console.log("Square of 4 :",square(4));
+
---
+++ javaScript/simple.js
@@ -0,0 +1,7 @@
+let name="Nikita";
+const age=21;
+var course="B.Tech";
+
+console.log("Name :",name);
+console.log("Age :",age);
+console.log("Course :",course);
